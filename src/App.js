be20fc6283dd42bbb14531bc8f966a90e4e03046
ReginaKidess <rkides01@ucy.ac.cy>
2025-06-06 15:18:47 +0200

    Set up environment, added application dependencies and styling (with tailwind).

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react' //rafce from es7+ extension
+import './App.css';
+
+const App = () => {
+  return (
+    <h1 className="underline text-3xl">App</h1>
+
+//to check that tailwind is working
+//     <div className="bg-red-200 text-center p-4 text-xl font-bold">
+//     Tailwind is working 🎉
+//     </div>
+  )
+}
+
+export default App
