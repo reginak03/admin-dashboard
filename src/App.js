1297ec006bfb2289bf54e35047580d896e92abae
ReginaKidess <rkides01@ucy.ac.cy>
2025-06-08 22:35:23 +0200

    Implemented Homepage section in Ecommerce.jsx

--- src/App.js
+++ src/App.js
@@ -47,37 +47,36 @@ const App = () => {
                     <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                         <Navbar />
                     </div>
-                </div>
 
-                <div>
-                    <Routes>
-                        {/* dashboard */}
-                        <Route path="/" element="Ecommerce" />
-                        <Route path="/ecommerce" element="Ecommerce" />
+                    <div>
+                        <Routes>
+                            {/* dashboard */}
+                            <Route path="/" element="Ecommerce" />
+                            <Route path="/ecommerce" element={<Ecommerce />} />
 
-{/* initially they were strings, after creating the components they were changed (e.g. from ...element="Order"/> to ...element={<Order />} />) using option button*/}
-                        {/* pages */}
-                        <Route path="/orders" element={<Orders />} />
-                        <Route path="/employees" element={<Employees />} />
-                        <Route path="/customers" element={<Customers />} />
+                            {/* initially they were strings, after creating the components they were changed (e.g. from ...element="Order"/> to ...element={<Order />} />) using option button*/}
+                            {/* pages */}
+                            <Route path="/orders" element={<Orders />} />
+                            <Route path="/employees" element={<Employees />} />
+                            <Route path="/customers" element={<Customers />} />
 
-                        {/* apps */}
-                        <Route path="/kanban" element={<Kanban />} />
-                        <Route path="/editor" element={<Editor />} />
-                        <Route path="/calendar" element={<Calendar />} />
-                        <Route path="/color-picker" element={<ColorPicker />} />
+                            {/* apps */}
+                            <Route path="/kanban" element={<Kanban />} />
+                            <Route path="/editor" element={<Editor />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path="/color-picker" element={<ColorPicker />} />
 
-                        {/* charts */}
-                        <Route path="/line" element={<Line />} />
-                        <Route path="/area" element={<Area />} />
-                        <Route path="/bar" element={<Bar />} />
-                        <Route path="/pie" element={<Pie />} />
-                        <Route path="/financial" element={<Financial />} />
-                        <Route path="/color-mapping" element={<ColorMapping />} />
-                        <Route path="/pyramid" element={<Pyramid />} />
-                        <Route path="/stacked" element={<Stacked />} />
-
-                    </Routes>
+                            {/* charts */}
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
+                        </Routes>
+                    </div>
                 </div>
             </div>
         </BrowserRouter>
