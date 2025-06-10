95fd94f4624fcd1c652d40372bb4aa2a71de9e4a
ReginaKidess <rkides01@ucy.ac.cy>
2025-06-10 15:19:11 +0200

    Created Line and Area charts.

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const LineChart = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>LineChart</div>
+    <ChartComponent id="line-chart" height="420px" primaryXAxis={LinePrimaryXAxis} primaryYAxis={LinePrimaryYAxis} chartArea={{ border:{width:0} }} tooltip={{ enable:true }} background={ currentMode === 'Dark' ? '#33373E':'#fff' }> 
+    {/* without primaryXAxis & primaryYAxis we get the default values belonging to the specific data points. chartArea is used to remove the border of the chart. tooltip is to get information about each point on the chart. */}
+      <Inject services={ [LineSeries, DateTime, Legend, Tooltip] } />
+      {/* map over the data: */}
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
 export default LineChart
+
+//change chart text color when dark, currently not very readable!
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div>
+      <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+        <Header title="Inflation Rate" category="Chart" />
+        <div className="w-full">
+          <LineChart /> {/* go to LineChart component */}
+        </div>
+      </div>
+    </div>
   )
 }
 
