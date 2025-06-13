425315960c1167b3a6c20820403795d3f6ededc1
ReginaKidess <rkides01@ucy.ac.cy>
2025-06-13 17:55:50 +0200

    Styling fixes.

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,13 +3,15 @@ import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, 
 
 import { scheduleData } from '../data/dummy';
 import { Header } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Calendar = () => {
+  const { currentMode } = useStateContext();
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg"> {/* this is the entire card */}
       <Header title="Calendar" category="App" />
-      <ScheduleComponent height="650px" eventSettings={{ dataSource:scheduleData }} selectedDate={new Date(2025,0,10)}>
+      <ScheduleComponent height="650px" eventSettings={{ dataSource:scheduleData }} selectedDate={new Date(2025,0,10)} className={currentMode === 'Dark' ? 'dark' : 'light'}>
         <Inject services={ [Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop] } />
       </ScheduleComponent>
     </div>
